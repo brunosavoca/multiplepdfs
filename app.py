5aa5654ce97e775e34e12246fcbf03119b7d26a9
Bruno Savoca Albors <98908114+brunosavoca@users.noreply.github.com>
2023-10-17 21:40:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,127 +1,51 @@
+import os
 import streamlit as st
-from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
-import pandas as pd
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI as LangOpenAI
+import openai
 
-def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+# Streamlit Config
+st.set_page_config(page_title="StudyAid", page_icon="📚", layout="wide")
 
+# API Key
+openai_api_key = st.secrets["api_key"]
 
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-    return chunks
+# Sidebar
+options = {
+    "home": "Home",
+    "document": "Document Interactions",
+}
 
+with st.sidebar:
+    option = st.selectbox("Select Option", list(options.values()))
 
-def get_vectorstore(text_chunks):
-    api_key = api_key
-    embeddings = OpenAIEmbeddings(api_key=api_key)
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vectorstore
+# Home Page
+if option == options["home"]:
+    st.header("Welcome to StudyAid 📚")
+    st.info("Select an option from the sidebar to get started.")
 
+# Document Interactions
+elif option == options["document"]:
+    try:
+        st.header("Analyze Your Documents")
+        pdf_file = st.file_uploader("Upload your PDF", type=["pdf"])
 
-
-def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
-
-
-
-
-def handle_userinput(user_question):
-    if st.session_state.conversation is not None:
-        response = st.session_state.conversation({'question': user_question})
-        st.session_state.chat_history = response['chat_history']
-
-        for i, message in enumerate(st.session_state.chat_history):
-            if i % 2 == 0:
-                st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-            else:
-                st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
-    else:
-        st.write("Please upload and process the PDFs first.")
-
-
-
-
-
-def main():
-    load_dotenv()
-    st.set_page_config(page_title="Chat with multiple PDFs",
-                       page_icon="logo1.png" )
-    st.write(css, unsafe_allow_html=True)
-
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
-
-    st.header("Chat with multiple PDFs :books:")
-    user_question = st.text_input("Ask a question about your documents:")
-    if user_question:
-        handle_userinput(user_question)
-
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        
-
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
-
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
-
-                # create vector store
-                vectorstore = get_vectorstore(text_chunks)
-
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
-                
-
-
-                # Clear chat history
-                st.session_state.chat_history = None
-                
-    if st.session_state.conversation is not None:
-        if st.session_state.chat_history is None:
-            # Greet the user
-            greeting = "Hello! How can I assist you with your documents?"
-            st.write(bot_template.replace("{{MSG}}", greeting), unsafe_allow_html=True)
-
-                
-
-
-if __name__ == '__main__':
-    main()
+        if pdf_file is not None:
+            reader = PdfReader(pdf_file)
+            raw_text = ''.join([page.extract_text() for page in reader.pages if page.extract_text()])
+            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+            texts = text_splitter.split_text(raw_text)
+            embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+            docsearch = FAISS.from_texts(texts, embeddings)
+            query = st.text_input("What do you want to know from this document?", "Provide a brief summary of this document.")
+            if st.button("Search"):
+                docs = docsearch.similarity_search(query)
+                chain = load_qa_chain(LangOpenAI(openai_api_key=openai_api_key))
+                answer = chain.run(input_documents=docs, question=query)
+                st.code(answer, language=None)
+    except:
+        st.error("An error occurred. Please try again.")
