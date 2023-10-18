d0cad595b4914ec321ada834259688988fb31dcd
Bruno Savoca Albors <98908114+brunosavoca@users.noreply.github.com>
2023-10-17 21:33:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    api_key = st.secrets["openai"]["api_key"]
+    api_key = api_key
     embeddings = OpenAIEmbeddings(api_key=api_key)
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
